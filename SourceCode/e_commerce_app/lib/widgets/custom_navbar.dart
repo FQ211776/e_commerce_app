4a25cf701d2770c0969844af73b902154ad5a60a
FQ211776 <fq211776@alumno.udb.edu.sv>
2022-04-25 21:53:41 -0600

    April 25, 2022 9:53 PM

--- SourceCode/e_commerce_app/lib/widgets/custom_navbar.dart
+++ SourceCode/e_commerce_app/lib/widgets/custom_navbar.dart
@@ -8,40 +8,36 @@ class CustomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
-      color: Colors.black,
-      child: SizedBox(
-        height: 70,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-                icon: const Icon(
-                  Icons.home,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                }),
-            IconButton(
-                icon: const Icon(
-                  Icons.shopping_cart,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/cart');
-                }),
-            IconButton(
-              icon: const Icon(
-                Icons.person,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/user');
-              },
-            )
-          ],
-        ),
-      ),
-    );
+        color: Colors.black,
+        child: SizedBox(
+            height: 70,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                      icon: const Icon(
+                        Icons.home,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      }),
+                  IconButton(
+                      icon: const Icon(
+                        Icons.shopping_cart,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/cart');
+                      }),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/user');
+                    },
+                  )])));
   }
 }
