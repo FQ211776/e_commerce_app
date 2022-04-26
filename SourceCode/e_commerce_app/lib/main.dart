538cbd981a849c5ed33c2e119913e64bfd0e9680
FQ211776 <fq211776@alumno.udb.edu.sv>
2022-04-25 19:26:30 -0600

    April 25, 2022 7:26 PM

--- SourceCode/e_commerce_app/lib/main.dart
+++ SourceCode/e_commerce_app/lib/main.dart
@@ -12,11 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Funeraria Monte Tabor',
       onGenerateRoute: AppRouter.onGeneratedRoute,
-      initialRoute: HomeScreen.routeName,
       //? === HomeScreen ===
-      home: HomeScreen(),
+      initialRoute: HomeScreen.routeName,
     );
   }
 }
