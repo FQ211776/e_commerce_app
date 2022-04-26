38a9d6ce05d8b1bfb5e3618cca570ca7b37e3655
FQ211776 <fq211776@alumno.udb.edu.sv>
2022-04-25 23:09:18 -0600

    April 25, 2022 11:09 PM

--- SourceCode/e_commerce_app/lib/screens/home/home_screen.dart
+++ SourceCode/e_commerce_app/lib/screens/home/home_screen.dart
@@ -28,7 +28,11 @@ class HomeScreen extends StatelessWidget {
       'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
     ];
 
-	final List<Widget> imageSliders = imgList
+
+	//? ************************************************************************
+	//? this will create a list of widgets, and each of this widgets will be a
+	//? container with the background image from the url list copiad above
+    final List<Widget> imageSliders = imgList
         .map((item) => Container(
               child: Container(
                 margin: EdgeInsets.all(5.0),
