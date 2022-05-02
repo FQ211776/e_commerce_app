a5350ce0624037a961f1687f02180bfe92bb2074
SilverBlue <fq211776@alumno.udb.edu.sv>
2022-05-02 11:14:59 -0600

    May 2, 2022 11:14 AM

--- SourceCode/e_commerce_app/lib/models/product_model.dart
+++ SourceCode/e_commerce_app/lib/models/product_model.dart
@@ -32,14 +32,15 @@ class Product extends Equatable {
       category: 'Soft Drinks',
       imageUrl:
           'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-      price: 2.99,
+      price: 3.99,
       isRecommended: true,
       isPopular: false,
     ),
     const Product(
       name: 'Soft Drink #2',
       category: 'Soft Drinks',
-      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
       price: 3.99,
       isRecommended: true,
       isPopular: false,
@@ -47,7 +48,8 @@ class Product extends Equatable {
     const Product(
       name: 'Soft Drink #3',
       category: 'Soft Drinks',
-      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
       price: 4.99,
       isRecommended: true,
       isPopular: false,
@@ -55,7 +57,8 @@ class Product extends Equatable {
     const Product(
       name: 'Soft Drink #4',
       category: 'Soft Drinks',
-      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
       price: 5.99,
       isRecommended: false,
       isPopular: true,
@@ -63,7 +66,8 @@ class Product extends Equatable {
     const Product(
       name: 'Soft Drink #5',
       category: 'Soft Drinks',
-      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
       price: 6.99,
       isRecommended: false,
       isPopular: true,
