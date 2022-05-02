d39a80137cd937310bb3385b4f90e24f1b424a01
SilverBlue <fq211776@alumno.udb.edu.sv>
2022-05-02 12:30:40 -0600

    May 2, 2022 12:30 PM

--- SourceCode/e_commerce_app/lib/widgets/widgets.dart
+++ SourceCode/e_commerce_app/lib/widgets/widgets.dart
@@ -1,6 +1,10 @@
+export 'package:e_commerce_app/widgets/product_card.dart';
+export 'package:e_commerce_app/widgets/section_title.dart';
+
 export 'custom_appbar.dart';
 export 'custom_navbar.dart';
 export 'hero_carousel_card.dart';
-export 'package:e_commerce_app/widgets/section_title.dart';
+
+
 
 
