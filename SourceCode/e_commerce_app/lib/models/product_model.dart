bddaf7117cf464a1b83c2115072f9aff18ad40a0
SilverBlue <fq211776@alumno.udb.edu.sv>
2022-05-02 10:59:05 -0600

    May 2, 2022 10:59 AM

--- SourceCode/e_commerce_app/lib/models/product_model.dart
+++ SourceCode/e_commerce_app/lib/models/product_model.dart
@@ -4,14 +4,68 @@ class Product extends Equatable {
   final String name;
   final String category;
   final String imageUrl;
-  final String price;
-  final String isRecommended;
-  final String isPopular;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
 
-  const Product(this.name, this.category, this.imageUrl, this.price,
-      this.isRecommended, this.isPopular);
+  const Product(
+      {required this.name,
+      required this.category,
+      required this.imageUrl,
+      required this.price,
+      required this.isRecommended,
+      required this.isPopular});
 
   @override
-  List<Object?> get props =>
-      [name, category, imageUrl, price, isRecommended, isPopular,];
+  List<Object?> get props => [
+        name,
+        category,
+        imageUrl,
+        price,
+        isRecommended,
+        isPopular,
+      ];
+
+  static List<Product> products = [
+    const Product(
+      name: 'Soft Drink #1',
+      category: 'Soft Drinks',
+      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Soft Drink #2',
+      category: 'Soft Drinks',
+      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      price: 3.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Soft Drink #3',
+      category: 'Soft Drinks',
+      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      price: 4.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    const Product(
+      name: 'Soft Drink #4',
+      category: 'Soft Drinks',
+      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      price: 5.99,
+      isRecommended: false,
+      isPopular: true,
+    ),
+    const Product(
+      name: 'Soft Drink #5',
+      category: 'Soft Drinks',
+      imageUrl: 'https://unsplash.com/photos/_ncPDMhXKJY',
+      price: 6.99,
+      isRecommended: false,
+      isPopular: true,
+    ),
+  ];
 }
