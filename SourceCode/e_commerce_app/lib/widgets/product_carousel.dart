90990af3c69ad24944f0fb8ff5a18c48e338e5e4
SilverBlue <fq211776@alumno.udb.edu.sv>
2022-05-02 17:18:17 -0600

    May 2, 2022 5:18 PM

--- SourceCode/e_commerce_app/lib/widgets/product_carousel.dart
+++ SourceCode/e_commerce_app/lib/widgets/product_carousel.dart
@@ -18,19 +18,21 @@ class ProductCarousel extends StatelessWidget {
       child: SizedBox(
         height: 165,
         child: ListView.builder(
-          shrinkWrap: true,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20.0,
-            vertical: 10.0,
-          ),
-          scrollDirection: Axis.horizontal,
-          itemCount: products.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(right: 5.0),
-              child: ProductCard(product: products[index]),
-            );
-          }),
+            shrinkWrap: true,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
+            scrollDirection: Axis.horizontal,
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 5.0),
+                child: ProductCard(
+                  product: products[index],
+                ),
+              );
+            }),
       ),
     );
   }
