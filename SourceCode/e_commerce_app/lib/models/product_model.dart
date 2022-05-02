9f7c39700b24627847b36c56bc5b55569aac6222
SilverBlue <fq211776@alumno.udb.edu.sv>
2022-05-02 10:44:22 -0600

    May 2, 2022 10:44 AM

--- SourceCode/e_commerce_app/lib/models/product_model.dart
+++ SourceCode/e_commerce_app/lib/models/product_model.dart
@@ -8,8 +8,10 @@ class Product extends Equatable {
   final String isRecommended;
   final String isPopular;
 
-  Product(this.name, this.category, this.imageUrl, this.price, this.isRecommended, this.isPopular);
+  const Product(this.name, this.category, this.imageUrl, this.price,
+      this.isRecommended, this.isPopular);
 
   @override
-    List<Object?> get props => [];
+  List<Object?> get props =>
+      [name, category, imageUrl, price, isRecommended, isPopular,];
 }
