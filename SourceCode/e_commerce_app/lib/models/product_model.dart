3c4703bbd1691dd36d7383f8449f4712675548e4
SilverBlue <fq211776@alumno.udb.edu.sv>
2022-05-02 16:05:34 -0600

    May 2, 2022 4:05 PM

--- SourceCode/e_commerce_app/lib/models/product_model.dart
+++ SourceCode/e_commerce_app/lib/models/product_model.dart
@@ -32,7 +32,7 @@ class Product extends Equatable {
       category: 'Soft Drinks',
       imageUrl:
           'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-      price: 3.99,
+      price: 1.99,
       isRecommended: true,
       isPopular: false,
     ),
