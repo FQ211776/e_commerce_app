aab4ce4af6619c11e215cbd7bffe0b2c610a978b
FQ211776 <FQ211776@alumno.udb.edu.sv>
2022-05-11 19:44:51 -0600

    May 11, 2022 7:44 PM

--- SourceCode/e_commerce_app/lib/screens/catalog/catalog_screen.dart
+++ SourceCode/e_commerce_app/lib/screens/catalog/catalog_screen.dart
@@ -1,17 +1,20 @@
+import 'package:e_commerce_app/models/category_model.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/widgets.dart';
 
 class CatalogScreen extends StatelessWidget {
-  const CatalogScreen({Key? key}) : super(key: key);
-
   static const String routeName = '/catalog';
 
-  static Route route() {
+  static Route route({required Category category}) {
     return MaterialPageRoute(
         settings: const RouteSettings(name: routeName),
-        builder: (_) => const CatalogScreen());
+        builder: (_) => CatalogScreen(category: category));
   }
+
+  final Category category;
+  // ignore: use_key_in_widget_constructors
+  const CatalogScreen({required this.category});
 
   @override
   Widget build(BuildContext context) {
