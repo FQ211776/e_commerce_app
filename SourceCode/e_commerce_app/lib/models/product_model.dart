52aa733f6409abb64afffbbbc5184b510c0d0166
FQ211776 <FQ211776@alumno.udb.edu.sv>
2022-05-11 22:29:17 -0600

    May 11, 2022 10:29 PM

--- SourceCode/e_commerce_app/lib/models/product_model.dart
+++ SourceCode/e_commerce_app/lib/models/product_model.dart
@@ -91,6 +91,15 @@ class Product extends Equatable {
       isRecommended: false,
       isPopular: false,
     ),
+	    const Product(
+      name: 'Smoothies #777',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 1.99,
+      isRecommended: false,
+      isPopular: false,
+    ),
     const Product(
       name: 'Smoothies #2',
       category: 'Smoothies',
